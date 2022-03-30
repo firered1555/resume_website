013338d12c881e6d788d4987b4ac0d09f9ec690b
firered1555 <47393765+firered1555@users.noreply.github.com>
2022-03-29 22:23:56 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteTitle: 'Portfolio', // <title>
+  manifestName: 'Portfolio',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Daniel Saviet',
+  subHeading: 'Full Time Database Specialist, Aspiring Software Developer',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/firered1555',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:ds8653@desales.edu',
+    },
+  ],
+  phone: '(570) 234-6556',
+  address: '305 Del Sol, Sunnyvale, CA',
+};
